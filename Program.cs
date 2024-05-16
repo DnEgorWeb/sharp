7430d83e41b88676a443d75fe391d0c0378514a5
Egor <egor19942012@gmail.com>
2024-05-16 21:14:45 +0200

    Add console and conditions topics

--- Program.cs
+++ Program.cs
@@ -4,8 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        // new Lesson2Variable.Variable();
-        // new ConsoleSolution.ConsoleApp().sayHello();
-        new Lesson3TypeConversions.TypeConverions().parsingFunctions();
+        new ConsoleSolution.ConsoleApp().mixOutput();
     }
 }
